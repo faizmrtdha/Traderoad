38eebd9fcf3d92bff318e41a8c5bad3c19304f5e
faizmrtdha <faizmrtdha99@gmail.com>
2021-09-13 18:56:42 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-$blog_posts = [
-    [
-        "title" => "Judul A",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, minus. Illum consequatur repudiandae ipsum ipsam nostrum quo at fugiat, vero ad id soluta. Et, ea laboriosam aliquid ut suscipit deleniti reprehenderit quaerat eligendi laborum saepe officia officiis atque doloribus tempore magnam quia fugiat ipsam eaque molestiae. Explicabo nisi, et quasi voluptatem laborum tempore non, unde ipsam asperiores facilis recusandae aut provident sed quis, numquam iusto deleniti sunt esse quas nobis distinctio laboriosam! Porro consequuntur voluptatem optio et placeat nesciunt, laboriosam eius quidem vel minima enim est! Asperiores odit, iure id sapiente quaerat minus veniam esse nesciunt, officiis aliquid aspernatur officia."
-    ],
-    [
-        "title" => "Judul B",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, minus. Illum consequatur repudiandae
-        ipsum ipsam nostrum quo at fugiat, vero ad id soluta. Et, ea laboriosam aliquid ut suscipit deleniti
-        reprehenderit quaerat eligendi laborum saepe officia officiis atque doloribus tempore magnam quia fugiat ipsam
-        eaque molestiae. Explicabo nisi, et quasi voluptatem laborum tempore non, unde ipsam asperiores facilis
-        recusandae aut provident sed quis, numquam iusto deleniti sunt esse quas nobis distinctio laboriosam! Porro
-        consequuntur voluptatem optio et placeat nesciunt, laboriosam eius quidem vel minima enim est! Asperiores odit,
-        iure id sapiente quaerat minus veniam esse nesciunt, officiis aliquid aspernatur officia."
-    ]
-    ];
     return view('index', [
         // "posts" => product::all()
     ]);
