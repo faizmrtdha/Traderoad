37028c21cd6b30c1bb6786a6bde72f8567b0514f
faizmrtdha <faizmrtdha99@gmail.com>
2021-09-13 19:11:40 +0700

    .

--- app/Models/product.php
+++ app/Models/product.php
@@ -12,7 +12,7 @@ class product
         [
             "judul" => "Coconut Shell",
             "slug" => "Coconut Shell",
-            "desc"=> "Arang berbahan dasar tempurung kelapa yg dibakar,arang ini menggunakan kelapa yg sudah tua dan
+            "desc" => "Arang berbahan dasar tempurung kelapa yg dibakar,arang ini menggunakan kelapa yg sudah tua dan
             mempunyai tekstur lebih padat,kandungan air sedikit,serta mempunyai nilai jual yg tinggi maka arang batok
             kelapa ini sangat dicari dan dibutuhkan sbg bahan dasar dalam industri arang briket."
         ],
--- resources/views/partials/konten_produk.blade.php
+++ resources/views/partials/konten_produk.blade.php
@@ -1,122 +1,120 @@
- <div class="isi d-md-flex flex-row justify-content-between mb-5">
-     <div class="thumbnail d-md-flex flex-wrap">
-         <div style="
+<div class="isi d-md-flex flex-row justify-content-between mb-5">
+    <div class="thumbnail d-md-flex flex-wrap">
+        <div style="
               --swiper-navigation-color: #fff;
               --swiper-pagination-color: #fff;
             " class="swiper mySwiper2">
-             <div class="swiper-wrapper">
-                 <div class="swiper-slide">
-                     <img src="/img/product/1.jpg" />
-                 </div>
-                 <div class="swiper-slide">
-                     <img src="/img/product/2.jpg" />
-                 </div>
-                 <div class="swiper-slide">
-                     <img src="/img/product/3.jpg" />
-                 </div>
-                 <div class="swiper-slide">
-                     <img src="/img/product/4.jpg" />
-                 </div>
-             </div>
-             <div class="swiper-button-next"></div>
-             <div class="swiper-button-prev"></div>
-         </div>
-         <div thumbsSlider="" class="swiper mySwiper">
-             <div class="swiper-wrapper">
-                 <div class="swiper-slide">
-                     <img src="/img/product/1.jpg" />
-                 </div>
-                 <div class="swiper-slide">
-                     <img src="/img/product/2.jpg" />
-                 </div>
-                 <div class="swiper-slide">
-                     <img src="/img/product/3.jpg" />
-                 </div>
-                 <div class="swiper-slide">
-                     <img src="/img/product/4.jpg" />
-                 </div>
-             </div>
-         </div>
-     </div>
-     <div class="details d-md-flex flex-column w-100">
-         @foreach ($judul as $j)
-         <h1 class="mb-4">
-
-             {{ $j['judul'] }}
-
-         </h1>
-         <h4>$1000</h4>
-         <p class="my-3">
-             {{ $j['desc'] }}
-
-         </p>
-         <h4>Specification</h4>
-         <table class="table">
-             <thead>
-                 <tr>
-                     <th scope="col">Parameter</th>
-                     <th scope="col">Unit</th>
-                     <th scope="col">Results</th>
-                     <th scope="col">Method</th>
-                 </tr>
-             </thead>
-             <tbody>
-                 <tr>
-                     <th scope="row">Moisture Content</th>
-                     <td>%</td>
-                     <td>2.72</td>
-                     <td>ASTM D 1762-84 (REAPPROVED 203)</td>
-                 </tr>
-                 <tr>
-                     <th scope="row">Ash Content</th>
-                     <td>%</td>
-                     <td>5.58</td>
-                     <td>ASTM D 1762-84 (REAPPROVED 203)</td>
-                 </tr>
-                 <tr>
-                     <th scope="row">Volatile Matter</th>
-                     <td>%</td>
-                     <td>8.42</td>
-                     <td>ASTM D 1762-84 (REAPPROVED 203)</td>
-                 </tr>
-                 <tr>
-                     <th scope="row">Fixed Carbon</th>
-                     <td>%</td>
-                     <td>83.28</td>
-                     <td>By Calculation</td>
-                 </tr>
-                 <tr>
-                     <th scope="row">Sulfur</th>
-                     <td>%</td>
-                     <td>0.08</td>
-                     <td>ASTM D 4239-14</td>
-                 </tr>
-                 <tr>
-                     <th scope="row">Carbon</th>
-                     <td>%</td>
-                     <td>83.20</td>
-                     <td>ASTM D 5373-08</td>
-                 </tr>
-                 <tr>
-                     <th scope="row">Hydrogen</th>
-                     <td>%</td>
-                     <td>1.63</td>
-                     <td>ASTM D 5373-08</td>
-                 </tr>
-                 <tr>
-                     <th scope="row">Nitrogen</th>
-                     <td>%</td>
-                     <td>0.88</td>
-                     <td>ASTM D 5373-08</td>
-                 </tr>
-                 <tr>
-                     <th scope="row">Oxigen</th>
-                     <td>%</td>
-                     <td>5.91</td>
-                     <td>ASTM D 3176-15</td>
-                 </tr>
-             </tbody>
-         </table>
-         @endforeach
-     </div>
- </div>
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="/img/product/1.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="/img/product/2.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="/img/product/3.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="/img/product/4.jpg" />
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+        <div thumbsSlider="" class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="/img/product/1.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="/img/product/2.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="/img/product/3.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="/img/product/4.jpg" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="details d-md-flex flex-column w-100">
+        @foreach ($judul as $j)
+        <h1 class="mb-1">
+            {{ $j['judul'] }}
+        </h1>
+        <p class="my-3">
+            {{ $j['desc'] }}
+        </p>
+        <h4>Specification</h4>
+        <div class="table-responsive">
+            <table class="table text-center">
+                <thead>
+                    <tr>
+                        <th scope="col">Parameter</th>
+                        <th scope="col">Unit</th>
+                        <th scope="col">Results</th>
+                        <th scope="col">Method</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Moisture Content</th>
+                        <td>%</td>
+                        <td>2.72</td>
+                        <td>ASTM D 1762-84 (REAPPROVED 203)</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Ash Content</th>
+                        <td>%</td>
+                        <td>5.58</td>
+                        <td>ASTM D 1762-84 (REAPPROVED 203)</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Volatile Matter</th>
+                        <td>%</td>
+                        <td>8.42</td>
+                        <td>ASTM D 1762-84 (REAPPROVED 203)</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Fixed Carbon</th>
+                        <td>%</td>
+                        <td>83.28</td>
+                        <td>By Calculation</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Sulfur</th>
+                        <td>%</td>
+                        <td>0.08</td>
+                        <td>ASTM D 4239-14</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Carbon</th>
+                        <td>%</td>
+                        <td>83.20</td>
+                        <td>ASTM D 5373-08</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Hydrogen</th>
+                        <td>%</td>
+                        <td>1.63</td>
+                        <td>ASTM D 5373-08</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Nitrogen</th>
+                        <td>%</td>
+                        <td>0.88</td>
+                        <td>ASTM D 5373-08</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Oxigen</th>
+                        <td>%</td>
+                        <td>5.91</td>
+                        <td>ASTM D 3176-15</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        @endforeach
+    </div>
+</div>
