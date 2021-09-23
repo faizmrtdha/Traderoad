93e573ab25254e375c202d784a32204ed37bf9b9
Faiz Murtadha Nur Wahhab <faizmrtdha99@gmail.com>
2021-09-23 13:57:22 +0700

    update warehouse css

--- resources/views/partials/warehouse.blade.php
+++ resources/views/partials/warehouse.blade.php
@@ -1,8 +1,26 @@
-{{-- <div class="container"> --}}
-<h2 class="text-center">Our Warehouse</h2>
-<div class="foto-ware">
-    <img src="img/wh1.jpg" alt="" class="img-fluid wh">
-    <img src="img/wh2.jpg" alt="" class="img-fluid wh">
-    <img src="img/wh3.jpg" alt="" class="img-fluid wh">
+{{-- <div class="container">
+    <h2 class="text-center">Our Warehouse</h2>
+    <div class="foto-ware">
+        <div class="row">
+            <img src="img/wh1.jpg" alt="" class="img-fluid wh">
+            <img src="img/wh2.jpg" alt="" class="img-fluid wh">
+            <img src="img/wh3.jpg" alt="" class="img-fluid wh">
+        </div>
+    </div>
+</div> --}}
+<div class="container">
+    <h2>Our Warehouse</h2>
+    <div class="foto-ware">
+        <div class="row">
+            <div class="col t-1">
+                <img src="img/wh1.jpg" alt="" class="wh">
+            </div>
+            <div class="col t-1">
+                <img src="img/wh2.jpg" alt="" class="wh">
+            </div>
+            <div class="col t-1">
+                <img src="img/wh3.jpg" alt="" class="wh img-fluid">
+            </div>
+        </div>
+    </div>
 </div>
-{{-- </div> --}}
